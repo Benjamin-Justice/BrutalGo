67531c88fdf371ffc9bbace8d0c388942838abf7
Sergej Tihonov <sergej.tihonov@brutalhack.com>
2016-10-09 23:03:14 +0200

    created a rough version of xml parser.

---
+++ Assets/Scripts/MapParser.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+using UnityEngine.Rendering;
+
+public class MapParser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    osm mapObject = loadMapFromXML();
+	    Debug.Log(mapObject.note);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private osm loadMapFromXML()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(osm));
+        return (osm) serializer.Deserialize(new XmlTextReader("Assets/XML/hh-small-response.xml"));
+    }
+}
